5387a3d1a9761c83cb87a293b4b401ae42b69f80
TUSHGOYAL10-deloitte <101543911+TUSHGOYAL10-deloitte@users.noreply.github.com>
2023-07-21 23:00:28 +0530

    Normal text

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+let name="Tushar";
+function App() { 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <nav>
+      <li>Home</li>
+      <li>About</li>
+      <li>Help</li>
+    </nav>
+      <div className="conatiner">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem maiores ex aspernatur dignissimos corrupti adipisci eum molestiae maxime ea dolorem eveniet ad odio recusandae iusto consectetur facere, officia beatae porro!
+      </div>
+    </>
+    
   );
 }
 
