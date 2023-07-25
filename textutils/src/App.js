92bb4298ff2293f0a25b7c8d170ab3e7683fb8bf
TUSHGOYAL10-deloitte <101543911+TUSHGOYAL10-deloitte@users.noreply.github.com>
2023-07-26 01:42:33 +0530

    Changes in index.html & app.js

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,19 +1,53 @@
-import './App.css';
+import "./App.css";
 
-let name="Tushar";
-function App() { 
+let name = "Tushar";
+function App() {
   return (
     <>
-    <nav>
-      <li>Home</li>
-      <li>About</li>
-      <li>Help</li>
-    </nav>
-      <div className="conatiner">
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem maiores ex aspernatur dignissimos corrupti adipisci eum molestiae maxime ea dolorem eveniet ad odio recusandae iusto consectetur facere, officia beatae porro!
-      </div>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/">
+            TextUtils
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/">
+                  About
+                </a>
+              </li>
+            </ul>
+            <form className="d-flex" role="search">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button className="btn btn-outline-success" type="submit">
+                Search
+              </button>
+            </form>
+          </div>
+        </div>
+      </nav>
     </>
-    
   );
 }
 
